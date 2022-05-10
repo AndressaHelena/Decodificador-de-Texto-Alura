c85ed7be547047936b99fe3877794282be3f6fae
AndressaHelena <handressa125@gmail.com>
2022-05-10 01:46:43 -0300

    Funcionalidades

--- script.js
+++ script.js
@@ -1,119 +1,77 @@
-/* Regras Codificador: 
-"e" é convertido para "enter" 
-"i" é convertido para "imes"
-"a" é convertido para "ai"
-"o" é convertido para "ober"
-"u" é convertido para "ufat"
-Apenas letras minúsculas
-Não permite acentuação
-Não permite números   
-*/
 
-/* Regras Decodificador: 
-<p>"enter" é convertido para "e"</p> 
-<p>"imes" é convertido para "i"</p> 
-<p>"ai" é convertido para "a"</p> 
-<p>"ober" é convertido para "o"</p> 
-<p>"ufat" é convertido para "u"</p> 
-<p>Apenas letras minúsculas</p> 
-<p>Não permite acentuação</p>   
-*/
 let textoDigitado = document.querySelector("#input-texto");
 let criptografar = document.querySelector("#btn-cripto");
-let decriptografar = document.querySelector("#btn-descripto");
-let copiarCripto = document.querySelector("#btn-copy");
-let criptoResultado = document.querySelector("#mensagem");
-let decriptoResultado = document.querySelector("#mensagem");
+let descriptografar = document.querySelector("#btn-descripto");
+let copiar = document.querySelector("#btn-copy");
+let resultado = document.querySelector("#mensagem");
+//let descriptoResultado = document.querySelector("#mensagem");
 let buttonLimpar = document.querySelector("#limpar");
 
 
-// Expressão regular que checa os caracteres de a-z e espaços em brancos até o fim da frase
-const regex = /^[a-z\s]+$/; 
+const regex = /^[a-z\s]+$/;
 
 function criptarTexto(event) {
   let texto = textoDigitado.value;
 
-  /*
-    ...E aqui na condição do IF
-    Se digitar alguma palavra com acento, maiúscula ou número
-    a condição vai barrar e exibirá um alerta
-    Caso contrário seguirá decriptando a frase
-  */
-  if (regex.test(texto) == true){
+  if (regex.test(texto) == true) {
 
-    
     let criptar1 = texto.replaceAll("e", "enter");
     let criptar2 = criptar1.replaceAll("i", "imes");
     let criptar3 = criptar2.replaceAll("a", "ai");
     let criptar4 = criptar3.replaceAll("u", "ufat");
     let criptar5 = criptar4.replaceAll("o", "ober");
-    
-    criptoResultado.value = criptar5;
+
+    resultado.value = criptar5;
     event.preventDefault();
-   
+
+
   } else {
-    
+
     alert("Digite uma frase válida, sem acentos, palavras maiúsculas ou números");
 
   }
- 
+
 }
 
-function decriptarTexto(event) {
+function descriptarTexto(event) {
   let texto = textoDigitado.value;
 
-  /*
-  Antes essa condição estava passando palavras maiúsculas
-  Agora com a correção tem o mesmo comportamento da outra condição
-  */
-  if (regex.test(texto) == true){
+  if (regex.test(texto) == true) {
 
     let decriptar1 = texto.replaceAll("enter", "e");
     let decriptar2 = decriptar1.replaceAll("imes", "i");
     let decriptar3 = decriptar2.replaceAll("ai", "a");
     let decriptar4 = decriptar3.replaceAll("ufat", "u");
     let decriptar5 = decriptar4.replaceAll("ober", "o");
-    
-    decriptoResultado.value = decriptar5;
+
+    resultado.value = decriptar5;
     event.preventDefault();
 
   } else {
-    
-    // Pode personalizar essa frase de alerta também
+
+
     alert("Digite uma frase válida, sem acentos ou palavras maiúsculas");
 
   }
-  
+
 }
+
+function limparTexto() {
+  document.getElementById("input-texto").value = ""
+  document.getElementById("mensagem").value = ""
+}
+
 function copiarTexto() {
-  let mensagem = criptoResultado.value;
-  navigator.clipboard.writeText(mensagem);
+  let copiar = mensagem;
+  navigator.clipboard.writeText(copiar.value);
 }
-
-
-
-function limparTexto(){
-  document.getElementById("input-texto").value=""
-  document.getElementById("mensagem").value=""
-  
- 
-}
-function mostra() {
-  var exibir = document.getElementById("input-texto");
-  if (exibir.style.visibility === 'hidden') {
-    exibir.mensagem.display = 'block';
-  } else {
-    exibir.style.visibility='hidden';
-  }
-}
-
 
 criptografar.onclick = criptarTexto;
 
-descriptografar.onclick = decriptarTexto;
+descriptografar.onclick = descriptarTexto;
 
-copiarCripto.onclick = copiarTexto;
-
-copiarDecripto.onclick = copiarTexto;
+copiar.onclick = copiarTexto;
 
 buttonLimpar.onclick = limparTexto;
+
+
